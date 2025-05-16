c82277c68e261438e5a504eec534fafc6b2613bd
ruben <ruben>
2025-05-16 17:25:29 +0200

    navbar responsive

--- App/Views/partials/navbar_admin.php
+++ App/Views/partials/navbar_admin.php
@@ -1,15 +1,14 @@
-<?php
-$saldo = $_SESSION['user']['saldo'] ?? null;
-?>
-<!-- Navbar -->
+<?php $saldo = $_SESSION['user']['saldo'] ?? null; ?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand fw-bold" href="/">ADMIN</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
+
+        <div class="collapse navbar-collapse justify-content-end text-center text-lg-start" id="navbarNav">
+            <ul class="navbar-nav me-lg-3">
                 <li class="nav-item">
                     <a class="nav-link" href="/conciertos">CONCIERTOS</a>
                 </li>
@@ -19,20 +18,17 @@ $saldo = $_SESSION['user']['saldo'] ?? null;
                 <li class="nav-item">
                     <a class="nav-link" href="/crea-concert">NUEVO CONCIERTO</a>
                 </li>
-                <?php if ($saldo !== null): ?>
-                    <li class="nav-item">
-                        <a href="/saldo" class="btn btn-outline-success ms-3">
-                            Saldo: <?= number_format($saldo, 2) ?>€
-                        </a>
-                    </li>
-                <?php endif; ?>
-                <li class="nav-item">
-                    <a href="/profile" class="btn btn-primary ms-2">Perfil</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/logout" class="btn btn-danger ms-3">Logout</a>
-                </li>
             </ul>
+
+            <div class="d-flex flex-column flex-lg-row align-items-center gap-2 w-auto w-sm-100 w-lg-auto">
+                <?php if ($saldo !== null): ?>
+                    <a href="/saldo" class="btn btn-outline-success w-100 w-lg-auto">
+                        <?= number_format($saldo, 2) ?>€
+                    </a>
+                <?php endif; ?>
+                <a href="/profile" class="btn btn-primary w-100 w-lg-auto">Perfil</a>
+                <a href="/logout" class="btn btn-danger w-100 w-lg-auto">Logout</a>
+            </div>
         </div>
     </div>
 </nav>
